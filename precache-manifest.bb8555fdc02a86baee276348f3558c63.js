23e3e5f1f0656cc63eb20c94ea103e749239681e
KevJackson2 <kevinjackson2@gmail.com>
2021-01-25 15:19:25 -0500

    Updates

---
+++ precache-manifest.bb8555fdc02a86baee276348f3558c63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3375ce339daeabe81e12978c88352a58",
+    "url": "/app-test/index.html"
+  },
+  {
+    "revision": "016527948168871209c9",
+    "url": "/app-test/static/css/main.6ecf7c0e.chunk.css"
+  },
+  {
+    "revision": "696709c598fa0eb13a7e",
+    "url": "/app-test/static/js/2.d199c52e.chunk.js"
+  },
+  {
+    "revision": "bb4c43b27eb8c651f5099d24504f50ed",
+    "url": "/app-test/static/js/2.d199c52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016527948168871209c9",
+    "url": "/app-test/static/js/main.e19a8d43.chunk.js"
+  },
+  {
+    "revision": "4887a2da8c97815e4848",
+    "url": "/app-test/static/js/runtime-main.6101b050.js"
+  },
+  {
+    "revision": "8ffe2dc95a03793f85aebd05bbecae5e",
+    "url": "/app-test/static/media/grad.8ffe2dc9.jpg"
+  }
+]);
